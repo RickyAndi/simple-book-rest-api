676f0e1fe940e0c5bf0eee04913e3d989bf36fdc
ricky <ricky10andika@gmail.com>
2017-07-16 00:09:38 +0700

    Add error handler

--- src/server.js
+++ src/server.js
@@ -5,9 +5,15 @@ const bodyParser = require('body-parser');
 const bookRoutes = require('./routes/book');
 
 server.use(bodyParser.json());
-
 server.use('/book', bookRoutes);
-
 server.get('/', require('./controllers/index').hello);
+
+server.use((err, req, res, next) => {
+  return res
+    .status(err.status)
+    .json({
+      error: err.message
+    });
+});
 
 module.exports = server;
