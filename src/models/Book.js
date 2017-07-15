19b4dbf726be97a0fb92b38452884e1efeacd3e9
ricky <ricky10andika@gmail.com>
2017-07-16 00:09:25 +0700

    Add more info

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,9 +1,18 @@
 const mongoose = require('../Mongoose');
 
 const schema = new mongoose.Schema({
-  title: String,
-  description: String,
-  author: String
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Book', schema);
