78eca71c1248c356bf15cde2e738d8a295bf78a2
ricky <ricky10andika@gmail.com>
2017-07-16 12:21:53 +0700

    Modify error middleware
    - Parse error message as object or as string

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 const bookRoutes = require('./routes/book');
 
+server.use(expressValidator());
 server.use(bodyParser.json());
 server.use('/book', bookRoutes);
 server.get('/', require('./controllers/index').hello);
 
 server.use((err, req, res, next) => {
+  let errorMessage;
+
+  try {
+    errorMessage = JSON.parse(err.message);
+  } catch(error) {
+    errorMessage = err.message;
+  }
+  
   return res
     .status(err.status)
     .json({
-      error: err.message
+      error: errorMessage
     });
 });
 
